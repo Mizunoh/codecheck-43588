91fefa9533cb0886c09a6b37f11acb9b63888a65
mizuno.hiroko <mizuno.hiroko@tis.co.jp>
2018-06-19 15:00:01 +0900

    3の倍数の1000以下の非負整数の場合、 idiot

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -1,10 +1,28 @@
 package codecheck;
 
+
+
 public class App {
 	public static void main(String[] args) {
 		for (int i = 0, l = args.length; i < l; i++) {
-			String output = String.format("argv[%s]: %s", i, args[i]);
+			//String output = String.format("argv[%s]: %s", i, args[i]);
+			String output = getResult(args[i]);
 			System.out.println(output);
 		}
+	}
+
+	private static String getResult(String param){
+		final String IDIOT = "idiot";
+
+		int i = Integer.parseInt(param);
+
+		if ((i % 3 == 0) && (i <= 1000)){
+			return IDIOT;
+		} else {
+			return null;
+		}
+
+
+
 	}
 }
