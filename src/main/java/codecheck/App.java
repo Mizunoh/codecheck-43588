42d4eaa30190a92e5164f1c7520896f77c329039
mizuno.hiroko <mizuno.hiroko@tis.co.jp>
2018-06-19 15:30:53 +0900

    3の倍数でかつ3のつく1000以下の非負整数の場合、dumb

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -14,19 +14,33 @@ public class App {
 	private static String getResult(String param){
 		final String IDIOT = "idiot";
 		final String STUPID = "stupid";
+		final String DUMB = "dumb";
 
 		int i = Integer.parseInt(param);
 
-		// 3の倍数の1000以下の非負整数の場合、 idiot
-		if ((i % 3 == 0) && (i <= 1000))
-			return IDIOT;
-		// 3のつく1000以下の非負整数の場合、 stupid
-		else if ((param.contains("3")) && (i <= 1000)){
-			return STUPID;
-		} else {
-			return null;
+		// 1000以下の非負整数か
+		if (i <= 1000){
+			// 3の倍数か
+			if (i % 3 == 0){
+				// 3がつくか
+				if (param.contains("3"))
+					return DUMB;
+				else
+					return IDIOT;
+			} else if (param.contains("3")){
+				return STUPID;
+			}
 		}
 
+//		// 3の倍数の1000以下の非負整数の場合、 idiot
+//		if ((i % 3 == 0) && (i <= 1000))
+//			return IDIOT;
+//			// 3のつく1000以下の非負整数の場合、 stupid
+//		else if ((param.contains("3")) && (i <= 1000)){
+//			return STUPID;
+//		} else {
+//			return null;
+//		}
 
 
 	}
